27fc8a7cac3f8cc1e0a1df205baf60251952e493
Mooooon-s <mmk990212@naver.com>
2023-06-16 10:58:53 +0900

    포트폴리오 깃허브 세팅

---
+++ Portpolio/Source/Portpolio/PortpolioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PortpolioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API APortpolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
